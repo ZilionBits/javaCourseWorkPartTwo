8f72072a93616cebceb474f9dc4301f05a59d64c
Darius Zilionis <Darius.Zilionis@aol.com>
2024-11-05 23:13:19 +0200

    initial commit

---
+++ UML/praktiniai_uml_and_packages/uml_02.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,11 @@
+public class Bot extends Player {
+
+    public void play(){
+        System.out.println("Bot");
+    };
+
+    public void addMove(String move){
+        System.out.println(move);
+    };
+
+}
