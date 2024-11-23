2b74a624803147e11dda4acd2f2dbc0e753c96f6
Darius Zilionis <Darius.Zilionis@aol.com>
2024-11-23 22:47:34 +0200

    initial commit

--- Spring/taskSimpleBooks/library/src/test/java/lt/stud/techin/library/LibraryApplicationTests.java
+++ Spring/taskSimpleBooks/library/src/test/java/lt/stud/techin/library/LibraryApplicationTests.java
@@ -1,13 +1,7 @@
 package lt.stud.techin.library;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LibraryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
